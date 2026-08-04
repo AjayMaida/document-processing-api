992551e4c7d3a0f5b9bfde9b716485d88c6d2423
Ajay Maida <ajaymaida98@gmail.com>
2026-08-04 12:36:43 +0530

    feat(auth): add user model and document ownership relationship

--- app/models/document.py
+++ app/models/document.py
@@ -1,9 +1,16 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime, String, func
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import DateTime, String, func , ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column,relationship
 
 from app.db.base import Base
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from app.models.user import User
+
+
 
 class Document(Base):
     __tablename__ = "documents"
@@ -13,7 +20,6 @@ class Document(Base):
     original_filename: Mapped[str] = mapped_column(String(255), nullable=False)
 
     stored_filename: Mapped[str] = mapped_column(String(255), nullable=False)
-
     status: Mapped[str] = mapped_column(
         String(50),
         default="uploaded",
@@ -24,4 +30,12 @@ class Document(Base):
         DateTime(timezone=True),
         server_default=func.now(),
         nullable=False,
+    )
+
+    user_id: Mapped[int] = mapped_column(
+    ForeignKey("users.id"),
+    nullable=False,
+    )
+    user: Mapped["User"] = relationship(
+        back_populates="documents"
     )
